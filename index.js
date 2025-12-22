0370bc942dfc25f825285d3470b521e61f86f37e
Saranya2991 <saara2991@gmail.com>
2025-12-22 02:40:03 +0000

    error check

--- index.js
+++ index.js
@@ -81,9 +81,9 @@ app.post("/sendemail",function(req, res){
 
             const PORT = process.env.PORT || 5000
 
-            if (process.env.NODE_ENV !== "production") {
+            
   app.listen(PORT, () => {
     console.log("Server Started...");
   });
-}
+
             console.log("running in port",PORT)
